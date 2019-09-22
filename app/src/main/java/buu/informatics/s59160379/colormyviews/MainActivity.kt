1a8618bdbb237001ef2ab95cf92cd3d8b06952c8
xxxDNxxx <beerurban@gmail.com>
2019-09-23 02:55:35 +0700

    Initial Project

---
+++ app/src/main/java/buu/informatics/s59160379/colormyviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package buu.informatics.s59160379.colormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
